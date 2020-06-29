cdaa5fe3e6715475fbe536bb2bb621597aae5456
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-29 15:07:08 -0300

    Added method PUT for update title, url and techs of a repository

--- src/app.js
+++ src/app.js
@@ -10,10 +10,12 @@ app.use(cors());
 
 const repositories = [];
 
+// List all repositories
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
+// Create a new repository
 app.post("/repositories", (request, response) => {
   const { title, url, techs, likes} = request.body;
 
@@ -30,8 +32,27 @@ app.post("/repositories", (request, response) => {
   return response.status(200).json(repository);
 });
 
+// Update the the title, url and techs of a repository
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id }  = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found.'});
+  }
+
+  const repository = {
+    id,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
